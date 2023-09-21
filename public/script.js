21a237696b53ddd12824e95dff0b5df3ca632009
kmal808 <kjmalley7@gmail.com>
2023-09-21 01:26:50 -1000

    refactor: make it dry

--- public/script.js
+++ public/script.js
@@ -1,88 +1,21 @@
-//$ Client side JS
+let button = document.getElementById('btn')
+let input = document.querySelector('.URL-input')
+let serverURL = 'http://localhost:2323'
 
-//note: Get the form data, POST it, and return the conversion
-
-//* Get the form element and add an event listener for submission
-const form = document.getElementById('conversion-form')
-form.addEventListener('submit', (event) => {
-	event.preventDefault()
-
-	const validation = document.getElementById('youtube-link').value
-
-	const conversionSettings = document.getElementById('conversionSettings').value
-
-	//* Get the selected conversion type (youtube or file)
-	const conversionType = document.querySelector(
-		'input[name="conversion-type"]:checked'
-	).value
-
-	//* Get the input value based on the selected conversion type
-	const inputElement = document.getElementById(
-		conversionType === 'youtube' ? 'youtube-link' : 'file-input'
-	)
-	const inputValue =
-		conversionType === 'youtube' ? inputElement.value : inputElement.files[0]
-
-	//* Create a FormData object to send the data
-	const formData = new FormData()
-	formData.append(conversionType, inputValue)
-	formData.append('conversionSettings', conversionSettings)
-
-	//* Make the POST request to the correct endpoint
-	const url =
-		conversionType === 'youtube' ? '/convert/youtube' : '/convert/file'
-
-	for (var pair of formData.entries()) {
-		console.log(pair[0] + ', ' + pair[1])
+button.addEventListener('click', () => {
+	if (!input.value) {
+		alert('Enter a valid YouTube URL')
+	} else {
+		downloadmp3(input.value)
 	}
-	fetch(url, {
-		method: 'POST',
-		body: formData,
-	})
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error('Conversion failed')
-			}
-			return response.blob()
-		})
-		.then((blob) => {
-			//* Create a download link for the converted file
-			const downloadLink = document.createElement('a')
-			downloadLink.href = URL.createObjectURL(blob)
-			downloadLink.download = 'output.mp3'
-			downloadLink.click()
-		})
-		.catch((error) => {
-			console.error('Fetch error:', error)
-			alert('Conversion failed.')
-		})
 })
 
-function validateYouTubeLink(validation) {
-	// RegEx to validate the input
-	var regex =
-		/^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})$/
-	return regex.test(validation)
-}
-
-function displayConversionResult(result) {
-	const resultContainer = document.getElementById('result-container')
-	const resultText = document.getElementById('result-text')
-	const copyButton = document.getElementById('copy-button')
-
-	resultContainer.style.display = 'block' // Show the result container
-	resultText.textContent = result // Set the result text
-
-	// Copy button functionality
-	copyButton.style.display = 'inline-block' // Show the copy button
-	copyButton.addEventListener('click', () => {
-		navigator.clipboard
-			.writeText(result) // Copy the result to the clipboard
-			.then(() => {
-				alert('Result copied to clipboard!')
-			})
-			.catch((error) => {
-				console.error('Failed to copy result:', error)
-			})
-	})
+async function downloadmp3(query) {
+	const res = await fetch(`${serverURL}/downloadmp3?url=${query}`)
+	if (res.status == 200) {
+		var a = document.createElement('a')
+		a.href = `${serverURL}/downloadmp3?url=${query}`
+		a.setAttribute('download', '')
+		a.click()
+	}
 }
--- server.js
+++ server.js
@@ -1,130 +1,52 @@
 const express = require('express')
-const multer = require('multer')
-const ytdl = require('ytdl-core')
-const ffmpeg = require('fluent-ffmpeg')
-const path = require('path')
 const cors = require('cors')
+const ytdl = require('ytdl-core')
+const path = require('path')
+const app = express()
 
 require('dotenv').config({ path: './config/.env' })
 
-const app = express()
 const port = process.env.PORT
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+	'/',
+	express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 })
+)
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.get('/downloadmp3', async (req, res, next) => {
+	try {
+		var url = req.query.url
+		if (!ytdl.validateURL(url)) {
+			return res.sendStatus(400)
+		}
+		let title = 'audio'
 
-app.get('/script.js', (req, res) => {
-	res.set('Content-Type', 'text/javascript')
-	res.sendFile(path.join(__dirname, 'public', 'script.js'))
-})
+		await ytdl.getBasicInfo(
+			url,
+			{
+				format: 'mp4',
+			},
+			(err, info) => {
+				if (err) throw err
+				title = info.player_response.videoDetails.title.replace(
+					/[^\x00-\x7F]/g,
+					''
+				)
+			}
+		)
 
-app.get('/style.css', (req, res) => {
-	res.set('Content-Type', 'text/css')
-	res.sendFile(path.join(__dirname, 'public', 'style.css'))
-})
-
-//$ Use multer for file uploads and downloads
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, './uploads/')
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.originalname)
-	},
-})
-
-const upload = multer({ storage: storage })
-
-//$ YouTube url conversion
-app.post('/convert/youtube', upload.none(), (req, res) => {
-	console.log('Received request to /convert/youtube')
-	console.log('Request body:', req.body)
-	const youtubeLink = req.body.youtube
-	const conversionSettings = req.body.conversionSettings
-
-	//* Validate the YouTube link
-	if (!validateYouTubeLink(youtubeLink)) {
-		console.log('YouTube link validation failed')
-		res.status(400).json({
-			message: 'Invalid YouTube URL',
-		})
-		return
+		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`)
+		ytdl(url, {
+			format: 'mp3',
+			filter: 'audioonly',
+		}).pipe(res)
+	} catch (err) {
+		console.error(err)
 	}
-
-	//* Perform the conversion using ytdl-core library
-	const stream = ytdl(youtubeLink, { filter: 'audioonly' })
-
-	//* Set the appropriate content type based on the conversion settings
-	let contentType = ''
-	if (conversionSettings === 'mp3') {
-		contentType = 'audio/mpeg'
-	} else if (conversionSettings === 'mp4') {
-		contentType = 'video/mp4'
-	} else {
-		res.status(400).json({ message: 'Invalid conversion settings.' })
-		return
-	}
-
-	res.set(
-		'Content-Disposition',
-		`attachment; filename="output.${conversionSettings}"`
-	)
-	res.set('Content-Type', contentType)
-
-	//* Pipe the converted file to the response
-	stream.pipe(res)
-
-	//* Get the download URL of the converted file
-	const downloadUrl = `/downloads/output.${conversionSettings}`
-
-	res.json({ message: 'Conversion Successful.', downloadUrl: downloadUrl })
 })
-
-//$ File upload conversion
-app.post('/convert/file', upload.single('file'), (req, res) => {
-	const file = req.file
-
-	if (!file) {
-		res.status(400).json({ message: 'No file uploaded' })
-		return
-	}
-
-	const filePath = file.path
-
-	ffmpeg(filePath)
-		.format('mp3')
-		.on('error', (err) => {
-			console.error(err)
-			res.status(500).json({ message: 'Conversion failed' })
-		})
-		.on('end', () => {
-			res.set('Content-Disposition', 'attachement; filename=:"output.mp3"')
-			res.set('Content-Type', 'audio/mpeg')
-
-			//* Pipe the converted file to the response
-			res.download(`${filePath}.mp3`, 'output.mp3', (err) => {
-				if (err) {
-					console.error(err)
-					res.status(500).json({ message: 'Download failed' })
-				}
-			})
-		})
-		.save(`${filePath}.mp3`)
-})
-
-function validateYouTubeLink(youtubeLink) {
-	//* RegEx to validate YouTube URL
-	const regex =
-		/^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})$/
-	return regex.test(youtubeLink)
-}
 
 app.listen(port, () => {
-	console.log(`Server listening on ${process.env.PORT}, better catch it!!`)
+	console.log(`Server is running 🏃 on ${port}, better catch it ⚾!!`)
 })
+
