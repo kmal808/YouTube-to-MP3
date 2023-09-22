49417041970662b12cc0367b46603826d9f8ccfa
kmal808 <kjmalley7@gmail.com>
2023-09-22 01:45:37 -1000

    perf: adding ffmpeg for conversion

--- public/script.js
+++ public/script.js
@@ -12,10 +12,17 @@ button.addEventListener('click', () => {
 
 async function downloadmp3(query) {
 	const res = await fetch(`${serverURL}/downloadmp3?url=${query}`)
-	if (res.status == 200) {
-		var a = document.createElement('a')
-		a.href = `${serverURL}/downloadmp3?url=${query}`
-		a.setAttribute('download', '')
+	if (res.status === 200) {
+		const blob = await res.blob()
+		const url = window.URL.createObjectURL(blob)
+		const a = document.createElement('a')
+		a.style.display = 'none'
+		a.href = url
+		a.download = 'output.mp3'
+		document.body.appendChild(a)
 		a.click()
+		window.URL.revokeObjectURL(url)
+	} else {
+		alert('Failed to download the audio. Please try again.')
 	}
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const ytdl = require('ytdl-core')
+const ffmpeg = require('fluent-ffmpeg')
 const path = require('path')
 const app = express()
 
@@ -20,33 +21,38 @@ app.get('/downloadmp3', async (req, res, next) => {
 		if (!ytdl.validateURL(url)) {
 			return res.sendStatus(400)
 		}
-		let title = 'audio'
 
-		await ytdl.getBasicInfo(
-			url,
-			{
-				format: 'mp4',
-			},
-			(err, info) => {
-				if (err) throw err
-				title = info.player_response.videoDetails.title.replace(
-					/[^\x00-\x7F]/g,
-					''
-				)
+		let title = 'audio'
+		await ytdl.getBasicInfo(url, { format: 'mp4' }, (err, info) => {
+			if (err) {
+				console.error('Error fetching video info', err)
+				return res.sendStatus(500)
 			}
-		)
+			let title = info.videoDetails.title.replace(/[^a-zA-Z0-9]/g, '_')
+		})
 
 		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`)
-		ytdl(url, {
-			format: 'mp3',
-			filter: 'audioonly',
-		}).pipe(res)
+
+		const audio = ytdl(url, { quality: 'highestaudio' })
+
+		ffmpeg()
+			.input(audio)
+			.audioCodec('libmp3lame')
+			.toFormat('mp3')
+			.on('end', () => {
+				console.log('Conversion finished.')
+			})
+			.on('error', (err) => {
+				console.error(err)
+				res.sendStatus(500)
+			})
+			.pipe(res, { end: true })
 	} catch (err) {
 		console.error(err)
+		res.sendStatus(500)
 	}
 })
 
 app.listen(port, () => {
 	console.log(`Server is running 🏃 on ${port}, better catch it ⚾!!`)
 })
-
